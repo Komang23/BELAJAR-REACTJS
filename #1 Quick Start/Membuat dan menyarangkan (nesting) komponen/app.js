13e9388c3f69f555fa691173a77832fc6043ad68
Komang23 <komangredana13@gamil.com>
2024-12-04 21:11:56 +0800

    Up

---
+++ #1 Quick Start/Membuat dan menyarangkan (nesting) komponen/app.js
@@ -0,0 +1,16 @@
+function MyButton(){
+    return(
+        <button>
+            Tombol
+        </button>
+    );
+}
+
+export default function MyApp(){
+    return(
+        <div>
+            <h1>Selamat Datang Di Aplikasi Saya</h1>
+            <MyButton/>
+        </div>
+    );
+}
